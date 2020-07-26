93c80089c83aa8ed32877bfd6db40ab4f4ef71b0
Viktar Rudzenia <viktarrudzenia@gmail.com>
2020-07-26 17:48:18 +0300

    feat: implement worked app

--- src/Components/Answers/Answers.js
+++ src/Components/Answers/Answers.js
@@ -4,28 +4,34 @@ import ListGroup from 'react-bootstrap/ListGroup'
 // import Answer from './Answer/Answer';
 import './Answers.scss';
 
-export default function Answers(props) { 
+export default function Answers(props) {
     return (
-        <>
-        <ListGroup className="Answers" as="ul">
-            {props.birdsData.map(item => {
-                return (
-                    <ListGroup.Item
-                        as="li"
-                        key={item.id}
-                        onClick={() => {
-                            props.changeAnswerColorState(item);
-                            setTimeout(() => {
-                                props.pickBird(item);
-                            }, 0);
-                        }}
-                    >
-                        <span className={`bg-${item.answerColorState} answer-circle`}></span>
-                        {item.name}
-                    </ListGroup.Item>
-                )
-            })}
-        </ListGroup>
-        </>
+            <ListGroup className="Answers" as="ul">
+                {props.birdsData.map(item => {
+                    return (
+                        <ListGroup.Item
+                            as="li"
+                            key={item.id}
+                            onClick={() => {
+                                if (props.isAnswerCanPicked) {
+                                    new Promise((resolve, reject) => {
+                                        props.pickBird(item);
+                                        resolve();
+                                    }).then(() => {
+                                        props.changeAnswerColorState(item);
+                                    }).then(() => {
+                                        if (props.defineIsAnswerPickedCorrect()) {
+                                            props.endLevelWithScore(props.defineScorePoints());
+                                        }
+                                    })
+                                }
+                            }}
+                        >
+                            <span className={`bg-${item.answerColorState} answer-circle`}></span>
+                            {item.name}
+                        </ListGroup.Item>
+                    )
+                })}
+            </ListGroup>
     )
 }
--- src/Components/Question/QuestionImg/QuestionImg.js
+++ src/Components/Question/QuestionImg/QuestionImg.js
@@ -4,6 +4,6 @@ import './QuestionImg.scss';
 
 export default function QuestionImg(props) {
     return (
-        <img className='QuestionImg' width="200px" height="150px" src={props.image} alt="bird" />
+        <img className='QuestionImg' width="200px" height="150px" src={props.isAnswerCanPicked ? "https://svgsilh.com/svg_v2/1363011.svg" : props.image} alt="bird" />
     )
 }
