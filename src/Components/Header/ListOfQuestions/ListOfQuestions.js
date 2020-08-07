706a1a3927ef7c1c9390d5d25090d241687bef07
Viktar Rudzenia <viktarrudzenia@gmail.com>
2020-08-07 13:55:18 +0300

    feat: add some scss

--- src/Components/Header/ListOfQuestions/ListOfQuestions.js
+++ src/Components/Header/ListOfQuestions/ListOfQuestions.js
@@ -6,7 +6,7 @@ export default function ListOfQuestions(props) {
     return (
         <ul className="ListOfQuestions row pagination">
             {props.data.map((item, index) => {
-                return <li key={index} className={`col-${12 / props.data.length} page-item ${props.currentArrayWithBirds === index ? 'active' : ''}`}><span className="page-link">{index}</span></li>
+                return <li key={index} className={`col-${12 / props.data.length} page-item ${props.currentArrayWithBirds === index ? 'active' : ''}`}><span className="page-link rounded">{index}</span></li>
             })}
         </ul>
     );
