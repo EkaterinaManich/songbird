922f229a2b9033d14d5d059487b4e1ed90b7a2f4
Ekaterina Manich <katenkarudzenia@gmail.com>
2020-08-08 17:47:50 +0300

    feat^ add some new styles

--- src/Components/GameOver/GameOver.js
+++ src/Components/GameOver/GameOver.js
@@ -5,8 +5,8 @@ import './GameOver.scss';
 export default function GameOver({ score, startGameFromBeginning, maxScore }) {
     return (
         <div className="GameOver">
-            <div>Поздравляем!</div>
-            <div>Вы прошли викторину и набрали {score} из {maxScore} возможных баллов</div>
+            <div className="congratilation">Поздравляем!</div>
+            <div className="game-result">Вы прошли викторину и набрали {score} из {maxScore} возможных баллов</div>
             {score === maxScore ? <div>Игра закончена</div> : <button onClick={startGameFromBeginning} className="btn btn-success">Попробовать ещё раз!</button>}
         </div>
     )
